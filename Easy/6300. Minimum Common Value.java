8e6b034f8f0dee9c74214531c7cfb27c209a6888
Mayur Bajait <92533816+mayurbajait@users.noreply.github.com>
2023-01-21 20:33:19 +0530

    Create 6300. Minimum Common Value.java

---
+++ Easy/6300. Minimum Common Value.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        int len = nums1.length+nums2.length;
+        int ans [] = new int[len];
+        
+        int idx = 0;
+        
+        for(int i = 0; i < nums1.length; i++){
+            for(int j = 0; j < nums2.length; j++){
+                if(nums1[i] == nums2[j]){
+                    ans[idx] = nums1[i];
+                    idx++;
+                }
+            }
+        }
+        
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < len; i++){
+            if(ans[i] < min && ans[i] != 0){
+                min = ans[i];
+            }
+        }
+        return min;
+        
+    }
+}
