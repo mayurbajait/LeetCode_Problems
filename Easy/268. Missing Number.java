4b3f7d742bb3e94251262cc2ace3b5e298a29e24
Mayur Bajait <92533816+mayurbajait@users.noreply.github.com>
2023-05-11 16:09:01 +0530

    Update 268. Missing Number.java

--- Easy/268. Missing Number.java
+++ Easy/268. Missing Number.java
@@ -1,18 +1,5 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        // Arrays.sort(nums);
-
-        // for(int i = 0; i < nums.length-1; i++){
-            
-        //     if(nums[i] == i){
-                
-        //     }
-        //     else{
-        //         return count;
-        //     }
-        // }
-        // return -1;
-
         int res;
         int n = nums.length;
         res = n*(n+1)/2;
